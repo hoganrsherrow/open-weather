2597283b8cde08f98a79105ecd52d53fc4fae661
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-08-30 14:44:39 -0500

    Created header, added JQuery and Bootstrap connections, styled header

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,18 @@ console.log("JS is working.");
 // API key from account with Open Weather
 var apiKey = "bb7c4152bdea4e092599aeb298e2c4ef";
 // API fetch to obtain current weather conditions from Open Weather
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat=36.174465&lon=-86.767960&appid=${apiKey}&units=imperial`)
-    .then((response) => {
-        if (response.ok) {
-            response.json().then((data) => {
-                console.log(data);
-            });
-        } else {
-            console.log(response.statusText);
-        }
-    });
+function callCurrentForecast() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=36.174465&lon=-86.767960&appid=${apiKey}&units=imperial`)
+        .then((response) => {
+            if (response.ok) {
+                response.json().then((data) => {
+                    console.log(data);
+                });
+            } else {
+                console.log(response.statusText);
+            }
+        });
+};
 // API fetch to obtain future forecasts. Limits requests to 5 days
 // fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=36.174465&lon=-86.767960&appid=${apiKey}&cnt=5&units=imperial`)
 //     .then( (response) => {
@@ -23,3 +25,9 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?lat=36.174465&lon=-86.767
 //             console.log(response.statusText);
 //         }
 //     });
+
+
+
+$(document).ready(() => {
+    console.log("The document is ready!");
+});
