0f227ff1ff99c924c868cd024016850a630d9e1c
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-08-30 16:47:59 -0500

    Added Search for a City functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,22 @@ function createSearchBtnEl() {
 function createForecastEl() {
     $("main").append(`<section class="forecast-container d-flex"></section>`);
     console.log("Forecast section appended");
-}
+};
+
+// Fetch for city lat and lon
+function fetchGeocode(city) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`)
+        .then( (response) => {
+            if (response.ok) {
+                response.json().then( (data) => {
+                    console.log(data);
+                })
+            } else {
+                console.log(response.statusText);
+                $(".search-for-a-city").prepend(`<div id="api-warning">Please enter a valid city name.</div>`);
+            }
+        })
+};
 
 // API fetch to obtain future forecasts. Limits requests to 5 days
 // fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=36.174465&lon=-86.767960&appid=${apiKey}&cnt=5&units=imperial`)
@@ -56,4 +71,13 @@ $(document).ready(() => {
     console.log("The document is ready!");
     createSearchForACityEl();
     createForecastEl();
+   // callCurrentForecast();
+    $("button").click( () => {
+        if ($("input")[0].value == "") {
+            $(".search-for-a-city").prepend(`<div id="api-warning">Please enter a valid city name.</div>`);
+        } else {
+            fetchGeocode($("input")[0].value);
+            $("input")[0].value = "";
+        }
+    });
 });
