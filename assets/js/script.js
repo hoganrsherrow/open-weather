3f1efc8589e7bbfd87f212fe59c81bf5d993f2c2
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-08-30 16:05:47 -0500

    created Search for a city and forecast sections along with inputs and labels

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-console.log("JS is working.");
 // API key from account with Open Weather
 var apiKey = "bb7c4152bdea4e092599aeb298e2c4ef";
 // API fetch to obtain current weather conditions from Open Weather
@@ -14,6 +13,31 @@ function callCurrentForecast() {
             }
         });
 };
+
+// Generate Search for a City section
+function createSearchForACityEl() {
+    $("main").append(`<section class="search-for-a-city"></section`);
+
+    createCitySearchInputEl();
+    createSearchBtnEl();
+};
+
+// Generate city search input
+function createCitySearchInputEl() {
+    $(".search-for-a-city").append(`<label for="city-input" class="form-label">Search for a City:</label><input type="text" class="form-control" id="city-input" placeholder="Nashville">`);
+};
+
+// Generate Search Button
+function createSearchBtnEl() {
+    $(".search-for-a-city").append(`<button type="button" class="btn btn-primary">Search</button>`);
+};
+
+// Generate forecast section
+function createForecastEl() {
+    $("main").append(`<section class="forecast-container d-flex"></section>`);
+    console.log("Forecast section appended");
+}
+
 // API fetch to obtain future forecasts. Limits requests to 5 days
 // fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=36.174465&lon=-86.767960&appid=${apiKey}&cnt=5&units=imperial`)
 //     .then( (response) => {
@@ -30,4 +54,6 @@ function callCurrentForecast() {
 
 $(document).ready(() => {
     console.log("The document is ready!");
+    createSearchForACityEl();
+    createForecastEl();
 });
