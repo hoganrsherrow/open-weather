06a828a7d9150c49ee5d51cb8fa83b0bc1f81785
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-08-30 13:20:01 -0500

    added initial API connections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
 console.log("JS is working.");
+// API key from account with Open Weather
+var apiKey = "bb7c4152bdea4e092599aeb298e2c4ef";
+// API fetch to obtain current weather conditions from Open Weather
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat=36.174465&lon=-86.767960&appid=${apiKey}&units=imperial`)
+    .then((response) => {
+        if (response.ok) {
+            response.json().then((data) => {
+                console.log(data);
+            });
+        } else {
+            console.log(response.statusText);
+        }
+    });
+// API fetch to obtain future forecasts. Limits requests to 5 days
+// fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=36.174465&lon=-86.767960&appid=${apiKey}&cnt=5&units=imperial`)
+//     .then( (response) => {
+//         if (response.ok) {
+//             response.json().then( (data) => {
+//                 console.log(data);
+//             });
+//         } else {
+//             console.log(response.statusText);
+//         }
+//     });
